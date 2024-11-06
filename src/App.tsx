9094a2c44d1b287bc765c77b9b13d484ec36be26
Ivan Kiral <kiralivan@gmail.com>
2024-11-06 13:35:59 +0100

    add featured article and event

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import Container from "./components/Container";
+import Divider from "./components/Divider";
+import FeaturedArticle from "./components/FeaturedArticle";
+import FeaturedEvent from "./components/FeaturedEvent";
 import Header from "./components/Header";
 import HeroImage from "./components/HeroImage";
+import Video from "./components/Video";
 import "./index.css";
 
 function App() {
@@ -7,6 +12,22 @@ function App() {
     <div>
       <Header />
       <HeroImage />
+      <Container>
+        <div className="pt-[104px] pb-40 inline-flex items-center ">
+          <Video />
+        </div>
+      </Container>
+      <div className="bg-creme">
+        <Container>
+          <FeaturedArticle />
+        </Container>
+      </div>
+      <Divider />
+      <div className="bg-creme">
+        <Container>
+          <FeaturedEvent />
+        </Container>
+      </div>
     </div>
   );
 }
---
+++ src/components/Divider.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+
+const Divider: FC = () => <hr className="w-full border-t border-[#D1D1D1]" />;
+
+export default Divider;
